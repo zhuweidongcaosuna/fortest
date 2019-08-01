8f2501b7fc8aa4910cbfe8e0d7a53bcedf4c77e5
zhuweidongcaosuna <1174996349@qq.com>
2019-08-01 23:14:47 +0800

    提交springboot测试

---
+++ src/main/java/com/sxtc/fortest/FortestApplication.java
@@ -0,0 +1,13 @@
+package com.sxtc.fortest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sxtc/fortest/FortestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sxtc.fortest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FortestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
